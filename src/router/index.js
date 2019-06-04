6f2517e07e5304ce24974b8b659bbb7da1a1222f
genzouw <genzouw@gmail.com>
2019-06-04 20:23:22 +0900

    developing...

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Playing from '@/components/Playing'
 import GameSet from '@/components/GameSet'
+import SignUp from '@/components/SignUp'
+import SignIn from '@/components/SignIn'
 
 Vue.use(Router)
 
@@ -23,6 +25,16 @@ export default new Router({
       path: '/gameSet/:countOfQuestions/:score',
       name: 'GameSet',
       component: GameSet
+    },
+    {
+      path: '/signUp',
+      name: 'SignUp',
+      component: SignUp
+    },
+    {
+      path: '/signIn',
+      name: 'SignIn',
+      component: SignIn
     }
   ]
 })
